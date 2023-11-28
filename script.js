bbb634b1bb8022b959bb8bad39d6d59bde779fde
BalajiVenkateshwaran <balajikthangam@gmail.com>
2023-11-29 01:53:50 +0530

    first commit

--- script.js
+++ script.js
@@ -1,66 +1,66 @@
 //For The Given JSON Iterate Over All for Loops (for, for-in, for-of, for-Each)
 
 // JSON iteration using for loop
-var data = { 
-    "name" : "Balaji Venkateshwaran",
-    "age": "29",
-    "location": "Chennai",
-    "current_Role" : "Full Stack Developer",
-    "previous_Role": "Frontend Developer",
-    "languageKnown": "JavaScript",
-    "others": "HTML, CSS, Bootstrap, Responsive Design",
-    "techUsedInProject" : "ReactJS, AntDesign, MaterialUI, TailWindCSS"
+var data = {
+  name: "Balaji Venkateshwaran",
+  age: "29",
+  location: "Chennai",
+  current_Role: "Full Stack Developer",
+  previous_Role: "Frontend Developer",
+  languageKnown: "JavaScript",
+  others: "HTML, CSS, Bootstrap, Responsive Design",
+  techUsedInProject: "ReactJS, AntDesign, MaterialUI, TailWindCSS",
 };
 var result = JSON.stringify(data);
-for(i=0; i<=result.length-1;i++){
-    console.log(result[i]);
+for (i = 0; i <= result.length - 1; i++) {
+  console.log(result[i]);
 }
-
-
 
 // JSON iteration using for-in loop
-var data = { 
-    "name" : "Balaji Venkateshwaran",
-    "age": "29",
-    "location": "Chennai",
-    "current_Role" : "Full Stack Developer",
-    "previous_Role": "Frontend Developer",
-    "languageKnown": "JavaScript",
-    "others": "HTML, CSS, Bootstrap, Responsive Design",
-    "techUsedInProject" : "ReactJS, AntDesign, MaterialUI, TailWindCSS"
+var data = {
+  name: "Balaji Venkateshwaran",
+  age: "29",
+  location: "Chennai",
+  current_Role: "Full Stack Developer",
+  previous_Role: "Frontend Developer",
+  languageKnown: "JavaScript",
+  others: "HTML, CSS, Bootstrap, Responsive Design",
+  techUsedInProject: "ReactJS, AntDesign, MaterialUI, TailWindCSS",
 };
-for (var key in data){
-    console.log(`${key} : ${data[key]}`);  // key:value will display in console 
+for (var key in data) {
+  console.log(`${key} : ${data[key]}`); // key:value will display in console
 }
 
-
-
 // JSON iteration using for-of loop
-var data = [{ 
-    "name" : "Balaji Venkateshwaran",
-    "age": "29",
-    "location": "Chennai",
-    "current_Role" : "Full Stack Developer",
-    "previous_Role": "Frontend Developer",
-    "languageKnown": "JavaScript",
-    "others": "HTML, CSS, Bootstrap, Responsive Design",
-    "techUsedInProject" : "ReactJS, AntDesign, MaterialUI, TailWindCSS"
-}];
+var data = [
+  {
+    name: "Balaji Venkateshwaran",
+    age: "29",
+    location: "Chennai",
+    current_Role: "Full Stack Developer",
+    previous_Role: "Frontend Developer",
+    languageKnown: "JavaScript",
+    others: "HTML, CSS, Bootstrap, Responsive Design",
+    techUsedInProject: "ReactJS, AntDesign, MaterialUI, TailWindCSS",
+  },
+];
 
-for(var k of data){
-        console.log(k);
-    }
+for (var k of data) {
+  console.log(k);
+}
 
 // JSON iteration using for-Each loop
-var data = [{ 
-    "name" : "Balaji Venkateshwaran",
-    "age": "29",
-    "location": "Chennai",
-    "current_Role" : "Full Stack Developer",
-    "previous_Role": "Frontend Developer",
-    "languageKnown": "JavaScript",
-    "others": "HTML, CSS, Bootstrap, Responsive Design",
-    "techUsedInProject" : "ReactJS, AntDesign, MaterialUI, TailWindCSS"
-}];
+var data = [
+  {
+    name: "Balaji Venkateshwaran",
+    age: "29",
+    location: "Chennai",
+    current_Role: "Full Stack Developer",
+    previous_Role: "Frontend Developer",
+    languageKnown: "JavaScript",
+    others: "HTML, CSS, Bootstrap, Responsive Design",
+    techUsedInProject: "ReactJS, AntDesign, MaterialUI, TailWindCSS",
+  },
+];
 
 data.forEach((keyvalue) => console.log(keyvalue));
