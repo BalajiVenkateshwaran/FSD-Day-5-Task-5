4837fb8f330c0b98538feef959660e2ca8538031
BalajiVenkateshwaran <balajikthangam@gmail.com>
2023-11-28 18:32:01 +0530

    first commit

---
+++ script1.js
@@ -0,0 +1,22 @@
+// Create Own Resume Data in JSON Format
+
+
+var resumeDetails = { 
+    "firstName" : "Balaji Venkateshwaran",
+    "lastName": "Panchatsaram",
+    "age": "29",
+    "location": "Chennai",
+    "mailID": "balajikthangam@gmail.com",
+    "department": "Mechanical Engg",
+    "College": "Anna University",
+    "yearOfPassedOut": "2016",
+    "current_Role" : "Full Stack Developer",
+    "previous_Role": "Frontend Developer",
+    "languageKnown": "JavaScript",
+    "others": "HTML, CSS, Bootstrap, Responsive Design",
+    "techUsedInProject" : "ReactJS, AntDesign, MaterialUI, TailWindCSS",
+    "backendTech": "MySQL, nodeJs, expressJS",
+    "workExperienceInIT": "6Months"
+};
+
+console.log(resumeDetails);  //Entire Resume Data Will Display In JSON Format
